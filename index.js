cb0abcb89917cfec7a1190af204baa981311b9e8
thanhdat.ly <thanhdat.ly@estuary.solutions>
2022-05-08 01:04:56 +0700

    init

--- index.js
+++ index.js
@@ -36,10 +36,10 @@ const sign = async () => {
 	const secretKey = fs.readFileSync("./keys/secret.key", "utf8");
 	const data = encrypt(JSON.stringify({ id: 1 }));
 	const payload = _.chunk(data, round(data.length / 3)).reduce(
-		(prev, current) => {
-			prev[crypto.randomBytes(4)] = current.join("");
-			return prev;
-		},
+		(prev, current, currentIndex) => ({
+			...prev,
+			[currentIndex]: current.join(""),
+		}),
 		{}
 	);
 	try {
@@ -65,7 +65,3 @@ const verify = async (token) => {
 	const data = decrypt(hash);
 	return JSON.parse(data);
 };
-
-verify(
-	"v4.public.eyLvv70477-977-9IjoiMjBkNGZlMGQ4ZGNmNzRhOSIsIu-_vSvvv71QIjoiOWI0ZGQzOTVkMWNlZDgxZiIsIu-_vVx1MDAxOO-_ve-_vSI6IjNjZTUzNjk3Zjk5MzMwYTEiLCJpYXQiOiIyMDIyLTA1LTA3VDE3OjQ3OjUxLjg5N1oiLCJleHAiOiIyMDIyLTA1LTA3VDE4OjQ3OjUxLjg5N1oifaDUGylPfZ-3qzsq7Yy9_3c--NP4daFJUm6J5CxgjmlCIDoSP0Ev55FnAJCQuUGi3dOVBZ6KHFJj431vWWJZrgE"
-);
